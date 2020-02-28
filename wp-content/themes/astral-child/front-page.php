4bfa31f9a0c255b3d0b48c07cc5e267497c518e7
Gregoire Maschio <gregmaschio@hotmail.com>
2020-02-28 14:13:37 -0500

    ajout du footer

--- wp-content/themes/astral-child/front-page.php
+++ wp-content/themes/astral-child/front-page.php
@@ -9,16 +9,16 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package underscores
+ * @package astral
  */
 
 get_header();
 ?>
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
+         
 		<?php
+
 		while ( have_posts() ) :
 			the_post();
 
@@ -29,8 +29,56 @@ get_header();
 				comments_template();
 			endif;
 
-		endwhile; // End of the loop.
+        endwhile; // End of the loop.
+        /////////////////////////////////NOUVELLE
+        echo '<h1>' . category_description(get_category_by_slug('nouvelle')) . '</h1>';
+        // The Query
+        $args = array(
+            "category_name" => "nouvelle",
+            "posts_per_page" => 3
+            // "orderby" => "date",
+            // "order" => "ASC"
+        );
+        $query1 = new WP_Query( $args );
+        
+        // The Loop
+        while ( $query1->have_posts() ) {
+            $query1->the_post();
+            echo '<h3>' . get_the_title() . '</h3>';
+            echo '<p>' . substr(get_the_excerpt(),0,200) . '</p>'; 
+        }
+        
+        /* Restore original Post Data 
+        * NB: Because we are using new WP_Query we aren't stomping on the 
+        * original $wp_query and it does not need to be reset with 
+        * wp_reset_query(). We just need to set the post data back up with
+        * wp_reset_postdata().
+        */
+        wp_reset_postdata();
+        
+        /////////////////////////////////EVENEMENT
+        echo '<h1>' . category_description(get_category_by_slug('evenement')) . '</h1>';        
+        /* The 2nd Query (without global var) */
+        $args2 = array(
+            "category_name" => "evenement",
+            "posts_per_page" => 10
+        );
+        $query2 = new WP_Query( $args2 );
+        
+        // The 2nd Loop
+        while ( $query2->have_posts() ) {
+            $query2->the_post();
+            echo '<h3>' . get_the_title( $query2->post->ID ) . '</h3>';
+            echo '<p>' . substr(get_the_excerpt(),0,200) . '</p>'; 
+
+        }
+        
+        // Restore original Post Data
+        wp_reset_postdata();
+        
+ 
 		?>
+    
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
